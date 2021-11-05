6d0f43f171117155ea66fd7beee9db180071b6a2
eko <muhammad.eko@admedika.co.id>
2021-11-05 10:14:45 +0700

    change vertical scroll jsgrid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/date-picker.css')}}">
+
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     @includeIf('layouts.admin.partials.css')
