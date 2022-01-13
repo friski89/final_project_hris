a2ac49c4739640f63c7ac9ce56aa329b4043525b
Muhammad Eko Priyatna <muhammad.eko@admedika.co.id>
2022-01-13 10:35:25 +0700

    remove mandatory

--- app/Models/TrainingHistory.php
+++ app/Models/TrainingHistory.php
@@ -13,6 +13,28 @@ class TrainingHistory extends Model
     use HasFactory;
     use Searchable;
 
+    // protected $fillable = [
+    //     'emp_no',
+    //     'employee_name',
+    //     'training_name',
+    //     'institution',
+    //     'start_date',
+    //     'years_of_training',
+    //     'training_duration',
+    //     'end_date',
+    //     'training_force',
+    //     'rating',
+    //     'trnevent_topic',
+    //     'trncourse_cost',
+    //     'trnevent_type',
+    //     'trn_flag',
+    //     'user_id',
+    //     'other_competencies_id',
+    //     'competence_fanctional_id',
+    //     'competence_leadership_id',
+    //     'competence_core_value_id',
+    // ];
+    
     protected $fillable = [
         'emp_no',
         'employee_name',
@@ -29,12 +51,9 @@ class TrainingHistory extends Model
         'trnevent_type',
         'trn_flag',
         'user_id',
-        'other_competencies_id',
-        'competence_fanctional_id',
-        'competence_leadership_id',
-        'competence_core_value_id',
     ];
 
+    
     protected $searchableFields = ['emp_no', 'employee_name'];
 
     protected $table = 'training_histories';
