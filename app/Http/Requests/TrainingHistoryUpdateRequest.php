528919d75fa67b132babbeca580e5b6eedf35f08
Muhammad Eko Priyatna <muhammad.eko@admedika.co.id>
2022-01-13 10:48:46 +0700

    test remove mandatory

--- app/Http/Requests/TrainingHistoryUpdateRequest.php
+++ app/Http/Requests/TrainingHistoryUpdateRequest.php
@@ -21,6 +21,42 @@ class TrainingHistoryUpdateRequest extends FormRequest
      *
      * @return array
      */
+    // public function rules()
+    // {
+    //     return [
+    //         'emp_no' => ['required', 'max:255', 'string'],
+    //         'employee_name' => ['required', 'max:255', 'string'],
+    //         'training_name' => ['required', 'max:255', 'string'],
+    //         'institution' => ['required', 'max:255', 'string'],
+    //         'start_date' => ['required', 'date'],
+    //         'years_of_training' => ['required', 'max:255', 'string'],
+    //         'training_duration' => ['required', 'max:255', 'string'],
+    //         'end_date' => ['required', 'date'],
+    //         'training_force' => ['required', 'max:255', 'string'],
+    //         'rating' => ['required', 'max:255', 'string'],
+    //         'trnevent_topic' => ['required', 'max:255', 'string'],
+    //         'trncourse_cost' => ['required', 'max:255', 'string'],
+    //         'trnevent_type' => ['required', 'max:255', 'string'],
+    //         'trn_flag' => ['required', 'max:255', 'string'],
+    //         'user_id' => ['required', 'exists:users,id'],
+    //         'other_competencies_id' => [
+    //             'nullable',
+    //             'exists:other_competencies,id',
+    //         ],
+    //         'competence_fanctional_id' => [
+    //             'nullable',
+    //             'exists:competence_fanctionals,id',
+    //         ],
+    //         'competence_leadership_id' => [
+    //             'nullable',
+    //             'exists:competence_leaderships,id',
+    //         ],
+    //         'competence_core_value_id' => [
+    //             'nullable',
+    //             'exists:competence_core_values,id',
+    //         ],
+    //     ];
+    // }
     public function rules()
     {
         return [
@@ -39,22 +75,6 @@ class TrainingHistoryUpdateRequest extends FormRequest
             'trnevent_type' => ['required', 'max:255', 'string'],
             'trn_flag' => ['required', 'max:255', 'string'],
             'user_id' => ['required', 'exists:users,id'],
-            'other_competencies_id' => [
-                'nullable',
-                'exists:other_competencies,id',
-            ],
-            'competence_fanctional_id' => [
-                'nullable',
-                'exists:competence_fanctionals,id',
-            ],
-            'competence_leadership_id' => [
-                'nullable',
-                'exists:competence_leaderships,id',
-            ],
-            'competence_core_value_id' => [
-                'nullable',
-                'exists:competence_core_values,id',
-            ],
         ];
     }
 }
