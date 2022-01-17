cea90e25f9958e91b397adafe16d779640fb892c
friski89 <noize.friski89@gmail.com>
2022-01-17 18:27:29 +0700

    udate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $project_title = '| AdMedika - HRIS';
+        $project_title = '| AdMedika - HCIS';
         View::share('title', $project_title);
     }
 }
