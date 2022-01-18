2d46b39615dae0956ed69ed67b537f72d82141b9
Muhammad Eko Priyatna <muhammad.eko@admedika.co.id>
2022-01-18 18:22:23 +0700

    change url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' =>env('APP_URL', 'https://erp.admedika.co.id'),
 
     'asset_url' => env('ASSET_URL', null),
 
